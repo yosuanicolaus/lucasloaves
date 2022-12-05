2419e71271557a6b3344942883c5844af915f998
Yosua Nicolaus <yosuanicolaus@gmail.com>
2022-12-06 10:36:47 +1100

    add more content to footer

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -1,11 +1,43 @@
+import { Link } from "react-router-dom";
+
 export function Footer() {
   const year = new Date().getFullYear();
 
   return (
-    <div className="bg-gray-800 text-white flex justify-center">
-      <a href="https://github.com/yosuanicolaus/lucasloaves">
-        &copy; Yosua Nicolaus {year}
-      </a>
-    </div>
+    <>
+      <div className="bg-orange-600 flex justify-evenly">
+        <div className="flex justify-center items-center">
+          <h1 className="text-2xl">Luca's Loaves</h1>
+        </div>
+        <ul className="text-sm py-5 text-slate-800">
+          <li>
+            <Link to={"/about"}>About</Link>
+          </li>
+          <li>
+            <Link to={"/careers"}>Careers</Link>
+          </li>
+          <li>
+            <Link to={"/order"}>Order</Link>
+          </li>
+          <li>
+            <Link to={"/contact"}>Contact</Link>
+          </li>
+        </ul>
+        <div className="flex justify-center items-center flex-col">
+          <b>Follow us:</b>
+          <div className="gap-3 flex">
+            <a href="#">twitter</a>
+            <a href="#">youtube</a>
+            <a href="#">github</a>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-orange-900 text-white flex justify-center">
+        <a href="https://github.com/yosuanicolaus/lucasloaves">
+          &copy; Yosua Nicolaus {year}
+        </a>
+      </div>
+    </>
   );
 }
