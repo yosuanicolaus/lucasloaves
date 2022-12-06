4eacfd78166382e21b77fde8238538cfbbda6a86
Yosua Nicolaus <yosuanicolaus@gmail.com>
2022-12-06 12:45:52 +1100

    create contact page (unfinished)

--- src/contact/index.tsx
+++ src/contact/index.tsx
@@ -1,7 +1,13 @@
 export function ContactPage() {
   return (
-    <div>
-      <div>contact page</div>
+    <div className="flex-grow p-5 text-center grid grid-cols-2">
+      <div>
+        <div>128 Chalmers St, Surry Hills NSW 2010</div>
+        <div>Phone: 9212 7799</div>
+        <div>Email address: info@lucasloaves.com.au</div>
+        <div> Opening</div> hours: 7am to 4 pm, 7 days a week.
+      </div>
+      <div>(Maps integration here)</div>
     </div>
   );
 }
