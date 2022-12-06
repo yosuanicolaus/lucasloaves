a086dff37d8b529701bc7af95a49d5b78dbfe38b
Yosua Nicolaus <yosuanicolaus@gmail.com>
2022-12-06 19:44:59 +1100

    extract all jobs from db, dynamically

--- src/careers/index.tsx
+++ src/careers/index.tsx
@@ -1,66 +1,24 @@
+import { useCollectionDataOnce } from "react-firebase-hooks/firestore";
+import { jobsQuery } from "../firebase";
+import { Job } from "../types";
+
 export function CareersPage() {
+  const [data, loading] = useCollectionDataOnce(jobsQuery);
+  const jobs = data as Job[];
+
   return (
     <div className="flex-grow p-5 text-center">
-      <div className="border p-5 mb-3">
-        <h2 className="font-bold">Baker</h2>
-        <div>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate
-          cumque perferendis eum et assumenda, repellendus voluptatem,
-          distinctio rerum eaque ab quae facilis ad? A commodi, consequuntur
-          voluptatum id laborum repellat laboriosam accusamus perspiciatis
-          repudiandae voluptatibus eveniet excepturi? Iste voluptatibus vel
-          autem saepe animi laudantium laboriosam, illo cumque neque ea?
-          Temporibus!
-        </div>
-        <button className="bg-blue-700 text-white rounded m-2 p-2">
-          Apply now
-        </button>
-      </div>
-      <div className="border p-5 mb-3">
-        <h2 className="font-bold">Washer</h2>
-        <div>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate
-          cumque perferendis eum et assumenda, repellendus voluptatem,
-          distinctio rerum eaque ab quae facilis ad? A commodi, consequuntur
-          voluptatum id laborum repellat laboriosam accusamus perspiciatis
-          repudiandae voluptatibus eveniet excepturi? Iste voluptatibus vel
-          autem saepe animi laudantium laboriosam, illo cumque neque ea?
-          Temporibus!
-        </div>
-        <button className="bg-blue-700 text-white rounded m-2 p-2">
-          Apply now
-        </button>
-      </div>
-      <div className="border p-5 mb-3">
-        <h2 className="font-bold">Pick Dish Guy/Girl</h2>
-        <div>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate
-          cumque perferendis eum et assumenda, repellendus voluptatem,
-          distinctio rerum eaque ab quae facilis ad? A commodi, consequuntur
-          voluptatum id laborum repellat laboriosam accusamus perspiciatis
-          repudiandae voluptatibus eveniet excepturi? Iste voluptatibus vel
-          autem saepe animi laudantium laboriosam, illo cumque neque ea?
-          Temporibus!
-        </div>
-        <button className="bg-blue-700 text-white rounded m-2 p-2">
-          Apply now
-        </button>
-      </div>
-      <div className="border p-5 mb-3">
-        <h2 className="font-bold">Waiter</h2>
-        <div>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate
-          cumque perferendis eum et assumenda, repellendus voluptatem,
-          distinctio rerum eaque ab quae facilis ad? A commodi, consequuntur
-          voluptatum id laborum repellat laboriosam accusamus perspiciatis
-          repudiandae voluptatibus eveniet excepturi? Iste voluptatibus vel
-          autem saepe animi laudantium laboriosam, illo cumque neque ea?
-          Temporibus!
-        </div>
-        <button className="bg-blue-700 text-white rounded m-2 p-2">
-          Apply now
-        </button>
-      </div>
+      {loading && <div>loading available jobs...</div>}
+      {jobs &&
+        jobs.map((job) => (
+          <div className="border p-5 mb-3" key={job.name}>
+            <h2 className="font-bold">{job.name}</h2>
+            <div>{job.description}</div>
+            <button className="bg-blue-700 text-white rounded m-2 p-2">
+              Apply now
+            </button>
+          </div>
+        ))}
     </div>
   );
 }
