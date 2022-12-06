9b341a89d8b34e603af872d8f9014915aab1a723
Yosua Nicolaus <yosuanicolaus@gmail.com>
2022-12-06 11:58:56 +1100

    create /about page contents and style it

--- src/about/index.tsx
+++ src/about/index.tsx
@@ -1,7 +1,41 @@
 export function AboutPage() {
   return (
-    <div>
-      <div>About page</div>
+    <div className="flex-grow p-5">
+      <div className="border p-5">
+        <h2 className="font-bold">About Luca's Loaves</h2>
+        <div>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate
+          cumque perferendis eum et assumenda, repellendus voluptatem,
+          distinctio rerum eaque ab quae facilis ad? A commodi, consequuntur
+          voluptatum id laborum repellat laboriosam accusamus perspiciatis
+          repudiandae voluptatibus eveniet excepturi? Iste voluptatibus vel
+          autem saepe animi laudantium laboriosam, illo cumque neque ea?
+          Temporibus!
+        </div>
+      </div>
+
+      <div className="border p-5 text-center">
+        <h2 className="font-bold">Why Luca's Loaves?</h2>
+        <ul>
+          <li>No Store/Commercial yeast</li>
+          <li>Hand kneaded and shaped in-house</li>
+          <li>Prepared over 14 - 17 hours</li>
+          <li>Organic flour</li>
+          <li>Easy to digest</li>
+        </ul>
+      </div>
+
+      <div className="border p-5 text-right">
+        <h2 className="font-bold">About Luca</h2>
+        <div>
+          Luca commenced his career as a lifeguard but was laid off. He found he
+          enjoyed making bread and experimented and in no time at all had built
+          up a thriving business. Lorem ipsum dolor sit, amet consectetur
+          adipisicing elit. Voluptates, voluptas perferendis et quam voluptatem
+          earum ut animi quas odit obcaecati aspernatur quos id adipisci velit
+          autem, placeat non asperiores facilis?
+        </div>
+      </div>
     </div>
   );
 }
