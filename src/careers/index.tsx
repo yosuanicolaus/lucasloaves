f268ae306e2778c95e19b2fb33e9e9901c536d22
Yosua Nicolaus <yosuanicolaus@gmail.com>
2022-12-06 12:06:46 +1100

    add content and style Careers page

--- src/careers/index.tsx
+++ src/careers/index.tsx
@@ -1,7 +1,66 @@
 export function CareersPage() {
   return (
-    <div>
-      <div>Careers page</div>
+    <div className="flex-grow p-5 text-center">
+      <div className="border p-5 mb-3">
+        <h2 className="font-bold">Baker</h2>
+        <div>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate
+          cumque perferendis eum et assumenda, repellendus voluptatem,
+          distinctio rerum eaque ab quae facilis ad? A commodi, consequuntur
+          voluptatum id laborum repellat laboriosam accusamus perspiciatis
+          repudiandae voluptatibus eveniet excepturi? Iste voluptatibus vel
+          autem saepe animi laudantium laboriosam, illo cumque neque ea?
+          Temporibus!
+        </div>
+        <button className="bg-blue-700 text-white rounded m-2 p-2">
+          Apply now
+        </button>
+      </div>
+      <div className="border p-5 mb-3">
+        <h2 className="font-bold">Washer</h2>
+        <div>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate
+          cumque perferendis eum et assumenda, repellendus voluptatem,
+          distinctio rerum eaque ab quae facilis ad? A commodi, consequuntur
+          voluptatum id laborum repellat laboriosam accusamus perspiciatis
+          repudiandae voluptatibus eveniet excepturi? Iste voluptatibus vel
+          autem saepe animi laudantium laboriosam, illo cumque neque ea?
+          Temporibus!
+        </div>
+        <button className="bg-blue-700 text-white rounded m-2 p-2">
+          Apply now
+        </button>
+      </div>
+      <div className="border p-5 mb-3">
+        <h2 className="font-bold">Pick Dish Guy/Girl</h2>
+        <div>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate
+          cumque perferendis eum et assumenda, repellendus voluptatem,
+          distinctio rerum eaque ab quae facilis ad? A commodi, consequuntur
+          voluptatum id laborum repellat laboriosam accusamus perspiciatis
+          repudiandae voluptatibus eveniet excepturi? Iste voluptatibus vel
+          autem saepe animi laudantium laboriosam, illo cumque neque ea?
+          Temporibus!
+        </div>
+        <button className="bg-blue-700 text-white rounded m-2 p-2">
+          Apply now
+        </button>
+      </div>
+      <div className="border p-5 mb-3">
+        <h2 className="font-bold">Waiter</h2>
+        <div>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate
+          cumque perferendis eum et assumenda, repellendus voluptatem,
+          distinctio rerum eaque ab quae facilis ad? A commodi, consequuntur
+          voluptatum id laborum repellat laboriosam accusamus perspiciatis
+          repudiandae voluptatibus eveniet excepturi? Iste voluptatibus vel
+          autem saepe animi laudantium laboriosam, illo cumque neque ea?
+          Temporibus!
+        </div>
+        <button className="bg-blue-700 text-white rounded m-2 p-2">
+          Apply now
+        </button>
+      </div>
     </div>
   );
 }
