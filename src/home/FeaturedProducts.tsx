822f6da8dddb5b9e1fda3972cd2062cb5ddf2a87
Yosua Nicolaus <yosuanicolaus@gmail.com>
2022-12-06 11:43:41 +1100

    add products and finish featuredproducts

--- src/home/FeaturedProducts.tsx
+++ src/home/FeaturedProducts.tsx
@@ -1,15 +1,34 @@
 import { useCollectionDataOnce } from "react-firebase-hooks/firestore";
 import { productsQuery } from "../firebase";
+import { Product } from "../types";
 
 export function FeaturedProducts() {
-  const [products, loading, error] = useCollectionDataOnce(productsQuery);
+  const [data, loading, error] = useCollectionDataOnce(productsQuery);
+  const products = data as Product[];
+  const featured = products.slice(0, 3);
+  if (error) return <div>Error: {error.message}</div>;
 
   return (
-    <div className="border border-black m-5 text-center">
-      <h1 className="font-bold text-lg">Featured Products</h1>
-      {loading && <div>loading product data...</div>}
-      {error && <div>Error: {error.message}</div>}
-      <div>{JSON.stringify(products)}</div>
+    <div className="border border-black m-5 text-center p-5">
+      <h1 className="font-bold text-lg py-4">Featured Products</h1>
+      {loading ? (
+        <div>loading product data...</div>
+      ) : (
+        <>
+          {featured.map((product) => (
+            <div key={product.name} className="text-left flex border p-3 mb-3">
+              <div className="flex-grow">
+                <div className="font-bold">{product.name}</div>
+                <div>{product.description}</div>
+              </div>
+              <div className="w-36 text-right">
+                <div>${product.price}</div>
+                <button className="bg-blue-400 rounded">Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </>
+      )}
     </div>
   );
 }
