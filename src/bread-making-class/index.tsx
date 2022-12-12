0d56f9f1d9c8dab8b8d10ac14d60d2428a2aad33
Yosua Nicolaus <yosuanicolaus@gmail.com>
2022-12-13 09:31:18 +1100

    Fix from Leonard's feedback: Bread making class
    - when Join button is clicked, add to shopping cart
    - change the video to any bread making class video

--- src/bread-making-class/index.tsx
+++ src/bread-making-class/index.tsx
@@ -1,4 +1,8 @@
+import { useCart } from "../cart";
+
 export function BreadMakingClassPage() {
+  const { addProduct } = useCart();
+
   return (
     <div className="flex-grow p-5">
       <div className="border p-5 flex flex-col justify-center items-center">
@@ -11,14 +15,24 @@ export function BreadMakingClassPage() {
           eligendi aut, illum quae similique a officiis reiciendis? Veritatis
           dolore velit non. Autem, blanditiis reiciendis?
         </div>
-        <button className="bg-blue-500 rounded p-2 m-2 text-white">
+        <button
+          className="bg-blue-500 rounded p-2 m-2 text-white"
+          onClick={() => {
+            addProduct({
+              name: "Bread Making Class",
+              price: 35,
+              description:
+                "Get access to the bread making class on the first saturday of every month.",
+            });
+          }}
+        >
           Join Now
         </button>
       </div>
       <div className="border p-5 flex justify-center items-center">
         <iframe
-          src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-          title="YouTube video player"
+          src="https://www.youtube.com/embed/FfsUm7qbC7o"
+          title="Bread Making Class Video"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           width={560}
           height={315}
